f5040933ea5e9c8f267620858a53791551004696
cycoconutz <johndyates@gmail.com>
2025-01-16 16:25:42 -0600

    removed testing port

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/karmatic", {
+// || "mongodb://localhost:27017/karmatic",
+mongoose.connect(process.env.MONGODB_URI  {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
