c9093f5eff4d0f030783b11192655915ed1262fa
Cyco <johndyates@gmail.com>
2026-03-04 14:55:51 -0600

    fix mongoose uri variable name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // || "mongodb://localhost:27017/karmatic",
-mongoose.connect(process.env.MONGODB_URI,  {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
