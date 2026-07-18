971d9b7a65c57709e1f8004d930a824e05dcb946
Cyco <johndyates@gmail.com>
2026-07-17 20:59:51 -0500

    fixed seeds

--- client/src/GlobalComponents/Footer/Footer.js
+++ client/src/GlobalComponents/Footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
             <img src='./assets/images/twitter.png' alt='twitter icon' />
             </Icons>
             <p>
-            &copy; Karmatic developed by <a href="www.johndyates.com">Danny Yates</a>, <a href="https://github.com/Stiltskinner">Ryan Thomas</a>, <a href="https://github.com/Tim-Zebra">Timothy Zalewski</a>, and <a href="https://github.com/billycwong19">William Wong</a>
+            &copy; Karmatic developed by <a href="https://www.johndyates.com">Danny Yates</a>, <a href="https://github.com/Stiltskinner">Ryan Thomas</a>, <a href="https://github.com/Tim-Zebra">Timothy Zalewski</a>, and <a href="https://github.com/billycwong19">William Wong</a>
             </p>
             
 
