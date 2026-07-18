cfa0e54733843b5a0a8a6c6b22168a627161f394
Cyco <johndyates@gmail.com>
2026-07-17 20:20:42 -0500

    updating mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // || "mongodb://localhost:27017/karmatic",
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
